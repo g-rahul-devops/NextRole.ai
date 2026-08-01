5e216b48ea6ae6ccc8f9585824bd9ec30b262558
rahul <rahul@gmail.com>
2026-08-02 01:08:10 +0530

    push

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [tanstackStart(), react(), tsconfigPaths()],
+  plugins: [tailwindcss(), tanstackStart(), react(), tsconfigPaths()],
   server: {
     host: '0.0.0.0',
     port: 3000,
